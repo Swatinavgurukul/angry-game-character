f1b456bbcdeac235459d3cd0cbf31acffcdeba88
Poojanavgurukul <Pooja17@navurukul.org>
2019-06-10 21:33:23 +0530

    deleted bird.js and box.js files because i set images so don't need these files.

--- sketch.js
+++ sketch.js
@@ -1,9 +1,6 @@
 let ground;
-// let angry_bird;
 let box;
 let bachground;
-// float x;
-
 function preload(){
     box = loadImage('image/equals.png')
     small_angry_bird = loadImage('image/small_bird.png');
@@ -13,31 +10,30 @@ function preload(){
     sling1=loadImage('image/sling1.png')
     sling2=loadImage('image/sling2.png')
 }
-
 function setup(){
     createCanvas(1000,500);
     ground = new Ground(0,height-25,width,50);
     big_angry_bird = loadImage('image/big_bird.png');
 }
-
 function draw(){
+    stroke(0);
     background(bachground);
     
     ground.show();
-
+    
     image(big_angry_bird,mouseX, mouseY,50,55);
-
+    
     image(sling1,80,279)
     image(sling2,52,270)
-    image(small_angry_bird,630,420)
+    image(small_angry_bird,627,420)
     
     
     image(big_pig,610,178);
     
-    image(box,680,375);
-    image(box,560,375);
-    image(box,620,275);
-
+    image(box,670,375);
+    image(box,565,375);
+    image(box,615,275);
+    
     image(small_pig,565,330);
     image(small_pig,705,330);
 
